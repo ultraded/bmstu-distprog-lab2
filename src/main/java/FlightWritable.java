aa5f62dd6c27fbd77e30c1ad18f9b83369c33990
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 12:15:35 +0300

    Scripted auto-commit on change (2021-12-18 12:15:35) by gitwatch.sh

--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -1,7 +1,18 @@
+import org.apache.hadoop.io.DataOutputBuffer;
 import org.apache.hadoop.io.Writable;
+
+import java.io.DataOutput;
+import java.io.IOException;
 
 public class FlightWritableComparable implements Writable {
 
+
+
+    
     @Override
-    public void write
+    public void write(DataOutput dataOutput) throw IOException {
+
+    }
+
+    @Override w
 }
