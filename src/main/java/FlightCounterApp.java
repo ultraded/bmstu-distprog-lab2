d2f02adb33c8f1a80b7818d6bc3d556e945a9693
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 07:59:35 +0300

    Scripted auto-commit on change (2021-12-18 07:59:35) by gitwatch.sh

--- src/main/java/FlightCounterApp.java
+++ src/main/java/FlightCounterApp.java
@@ -1,4 +1,11 @@
-
+import org.apache.hadoop.mapreduce.Job;
 
 public class FlightCounterApp {
+    public static void main(String[] args) throws Exception {
+        if (args.length !=3) {
+            System.err.println("Usage:");
+            System.exit(-1);
+        }
+        Job job = setJarByC
+    }
 }
