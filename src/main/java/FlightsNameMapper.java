ae485e2e0d62e0eb38cb0791ebf1401bb9da3ec1
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 13:22:09 +0300

    Scripted auto-commit on change (2021-12-18 13:22:09) by gitwatch.sh

--- src/main/java/FlightsNameMapper.java
+++ src/main/java/FlightsNameMapper.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 public class FlightsNameMapper extends Mapper<LongWritable, Text, FlightsWritableComparable, Text> {
 
-    private static final String DELIMINTER = "\","
-    private static final int AIRPORTID
+    private static final String DELIMINTER = "\",";
+    private static final int AIRPORT_ID_INDEX = 0, AIRPORT_NAME_INDEX = 1;
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() > 0) {
