fd052564c5bc5064c378cf0420872453e5cedf8a
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 13:32:36 +0300

    Scripted auto-commit on change (2021-12-18 13:32:36) by gitwatch.sh

--- src/main/java/FlightsMapper.java
+++ src/main/java/FlightsMapper.java
@@ -4,5 +4,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class FlightsMapper extends Mapper<LongWritable, Text > {
     private static final String DELIMITER = ",";
-    
+    private static final int AIPORT_ID_INDEX = 14, DELAY_INDEX = 17;
+    private static final bool AIRPORT_WC_IDENTIFICATION
 }
