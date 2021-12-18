04ddae83a3742d4edec286a9cdb725fb271bce1c
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 12:47:27 +0300

    Scripted auto-commit on change (2021-12-18 12:47:27) by gitwatch.sh

--- src/main/java/FlightsGroupingComparator.java
+++ src/main/java/FlightsGroupingComparator.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class FlightsGroupingComparator {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class FlightsGroupingComparator extends WritableComparator {
+
+    @Override
+    public int compare(WritableComparable a,WritableComparable b) {
+        FlightsWritableComparable firstFlight = (FlightsWritableComparable) a;
+        FlightsWritableComparable secondFlight = (FlightsWritableComparable) b;
+        return firstFlight.compareID(secondFlight);
+    }
 }
