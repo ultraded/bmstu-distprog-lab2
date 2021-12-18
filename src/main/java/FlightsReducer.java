e216e450c53c7c5214e5ee42c172e3721b21ee1f
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 13:09:46 +0300

    Scripted auto-commit on change (2021-12-18 13:09:46) by gitwatch.sh

--- src/main/java/FlightsReducer.java
+++ src/main/java/FlightsReducer.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class FlightsReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class FlightsReducer extends Reducer<FlightsWritableComparable, Text, Text, Text> {
+
+    @Override
+    private void reduce(FlightsWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text name = new Text(iter.next());
+        int count = 0;
+        float min = 0, max = 0, sum = 0;
+        while (iter.hasNext()) {
+            float currVal = Float.parseFloat((iter.next().toString()));
+            if (count == 0) {
+                min = currVal;
+                max= currVal;
+            } else {
+                if (currVal < min) min = currVal;
+                else if (currVal > max) max = currVal;
+                sum +=currVal;
+                count++;
+            }
+            if (count != 0) {
+                float avg = sum / count;
+                context.write(name, new Text("Min"));
+            }
+        }
+    }
 }
