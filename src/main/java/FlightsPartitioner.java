3f9bf3dc6b2c225763a56f8c882ce0a55a1712fb
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 13:46:18 +0300

    Scripted auto-commit on change (2021-12-18 13:46:18) by gitwatch.sh

--- src/main/java/FlightsPartitioner.java
+++ src/main/java/FlightsPartitioner.java
@@ -3,7 +3,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class FlightsPartitioner extends Partitioner<FlightsWritableComparable, Text> {
     @Override
-    public int getPartition(FlightsWritableComparable key, Text text, int i) {
-        return key.getID() % i;
+    public int getPartition(FlightsWritableComparable key, Text text, int modulo) {
+        return key.getID() % modulo;
     }
 }
