06454238a070e7e28a2dab5dcf519e7932ea1584
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 12:22:36 +0300

    Scripted auto-commit on change (2021-12-18 12:22:36) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -5,5 +5,5 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class FlightMapper extends Mapper<LongWritable, Text > {
 
     private int AID;
-    private float delay, 
+    private float delay,
 }
