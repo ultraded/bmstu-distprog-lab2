07e289b6a558cbf22746be54230c78601920582f
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 12:31:23 +0300

    Scripted auto-commit on change (2021-12-18 12:31:23) by gitwatch.sh

--- src/main/java/FlightsWritableComparable.java
+++ src/main/java/FlightsWritableComparable.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class FlightsWritableComparable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataOutput;
+
+public class FlightsWritableComparable implements WritableComparable {
+    private int aeroID, identity;
+
+    public FlightsWritableComparable(int aeroportID) {
+
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throw IOException {
+        dataOutput.writeInt(aeroportID);
+        data
+    }
 }
