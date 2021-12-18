b7d623e1d0843cf22870f587092723308e97d8c0
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 13:57:05 +0300

    Scripted auto-commit on change (2021-12-18 13:57:05) by gitwatch.sh

--- src/main/java/FlightDelayCounterApp.java
+++ src/main/java/FlightDelayCounterApp.java
@@ -1,17 +1,32 @@
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
-public class FlightCounterApp {
+public class FlightDelayCounterApp {
+
+    private static int NUM_REDUCE_TASKS = 2;
+
     public static void main(String[] args) throws Exception {
         if (args.length !=3) {
             System.err.println("Usage:");
             System.exit(-1);
         }
         Job job = Job.getInstance();
-        job.setJarByClass(FlightCounterApp.class);
-        job.setJobName("Flight count");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        
+        job.setJarByClass(FlightDelayCounterApp.class);
+        job.setJobName("Flight delay count");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsMapper.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(FlightsPartitioner.class);
+        job.setGroupingComparatorClass(FlightsGroupingComparator.class);
+        job.setReducerClass(FlightsReducer.class);
+        job.setMapOutputKeyClass(FlightsWritableComparable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setNumReduceTasks();
+
     }
 }
