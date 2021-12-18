abc054a96765dedee450de0f681e7316c0d3d98a
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 10:21:34 +0300

    Scripted auto-commit on change (2021-12-18 10:21:34) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,2 +1,7 @@
-public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class FlightMapper extends Mapper<LongWritable, Text > {
+
 }
--- src/main/java/FlightWritable.java
+++ src/main/java/FlightWritable.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class FlightWritable {
+public class FlightWritable {
 }
