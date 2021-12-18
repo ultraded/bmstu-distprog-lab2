2fc6fb4dc3599c89ddff94025297791bd8a1c71c
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 13:29:59 +0300

    Scripted auto-commit on change (2021-12-18 13:29:59) by gitwatch.sh

--- src/main/java/FlightsMapper.java
+++ src/main/java/FlightsMapper.java
@@ -4,5 +4,5 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class FlightsMapper extends Mapper<LongWritable, Text > {
     private static final String DELIMITER = ",";
-    private static final Strin
+    
 }
