85800777d86f711370ea7ab659ff1591d79d72bb
ultraded <andrey.chernenkov@mail.ru>
2021-12-18 13:15:58 +0300

    Scripted auto-commit on change (2021-12-18 13:15:58) by gitwatch.sh

--- src/main/java/FlightsNameMapper.java
+++ src/main/java/FlightsNameMapper.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class FlightsNameMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+public class FlightsNameMapper extends Mapper<LongWritable, Text, FlightsWritableComparable, Text {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) thr
+
 }
